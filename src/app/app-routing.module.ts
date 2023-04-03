ae67047200504fea85c170856d9058f35b396f0f
Kyu18 <keannulacambra18@gmail.com>
2023-04-03 20:25:00 +0800

    yot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ApprovalComponent } from './approval/approval.component';
+import { HeadnavComponent } from './headnav/headnav.component';
+import { SettingsComponent } from './settings/settings.component';
+import { UsersComponent } from './users/users.component';
+import { SummaryComponent } from './summary/summary.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:DashboardComponent, path: 'dashboard'
+  },
+  {
+    component:ApprovalComponent, path: 'approval'
+  },
+  {
+    component:SettingsComponent, path: 'settings'
+  },
+  {
+    component:UsersComponent, path: 'users'
+  },
+  {
+    component:SummaryComponent, path: 'summary'
+  },
+  {
+    component:DashboardComponent, path: 'dashboard'
+  },
+  {
+    component:LoginComponent, path: ''
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadnavComponent } from './headnav/headnav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ApprovalComponent } from './approval/approval.component';
+import { SettingsComponent } from './settings/settings.component';
+import { UsersComponent } from './users/users.component';
+import { SummaryComponent } from './summary/summary.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadnavComponent,
+    DashboardComponent,
+    ApprovalComponent,
+    SettingsComponent,
+    UsersComponent,
+    SummaryComponent,
+    LoginComponent,
+    LayoutComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
